16a371528b69b681a4faf79f19b7f790e7e6a638
Prince Shah <2406prince2006@gmail.com>
2025-10-24 11:52:41 +0530

    project setup done and venv created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src", 
+    version="0.0.1",
+    author="Prince Shah",
+    author_email="2406prince2006@gmail.com",
+    packages=find_packages()
+)
